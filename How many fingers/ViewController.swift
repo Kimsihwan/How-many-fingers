5904c9db5a01d6c8931c3c1340f9d3b3ce3c142e
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-27 15:48:11 +0900

    fingergame

--- How many fingers/ViewController.swift
+++ How many fingers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btn(_ sender: Any) {
+        let number = arc4random_uniform(6)
+        print(number)
+        
+        let numberString = String(number)
+        
+        if input.text == numberString {
+            result.text = "빙고"
+        } else {
+            result.text = "실패 다시하셈 결과 : \(number)"
+        }
+    }
+    
 }
 
